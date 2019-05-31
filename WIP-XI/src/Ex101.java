5e04b7b932a8ff08d43851e603153fb642e0a5b6
ThanachotD <32594801+ThanachotD@users.noreply.github.com>
2019-05-31 11:47:09 +0700

    Revert "Project"
    This reverts commit 90ebc2aade954ff36f2c700a5265bbd070a3efff.

--- WIP-XI/src/Ex101.java
+++ WIP-XI/src/Ex101.java
@@ -7,32 +7,31 @@ public class Ex101 {
         Scanner sc = new Scanner(System.in);
         Scanner member = new Scanner(System.in);
         Scanner my = new Scanner(System.in);
-        double priceT , priceV;
         
-        System.out.print("ราคาอาหารทั้งหมด : ");        
+
+        System.out.print("ราคาอาหารทั้งหมด");
         
         double price = sc.nextInt();
         
-        System.out.print("มีบัตรสมาชิกไหม : ");
+        System.out.print("มีบัตรสมาชิกไหม");
         
         boolean have = member.nextBoolean();
         
         if (have = true) {
-            priceT = price+ price*(5.00/100.00);
-           
-        }
+            double priceT = price*(5.00/100.00);
+            
             System.out.println("มีบัตรสมาชิก");
             
-            System.out.print("ชำระด้วยเงินสดไหม :");
+            System.out.println("ใช้เงินสดไหม");
             
-            boolean money = my.nextBoolean(); 
-        if(money = true){
-            priceV = price+(price*10.00/100.00);
-            System.out.print("ค่าอาหารทั้งหมด :" + priceV);
-        }
-        if (money = false){
-        priceV = price+(price*5.00/100.00);
-            System.out.print("ค่าอาหารทั้งหมด :" + priceV);
+            boolean money = my.nextBoolean();
+            
+            if (money = true){
+                System.out.println("ชำระด้วยเงินสด");
+                System.out.println(price-priceT);
+            }if (money = true) {
+                
+            }
         }
     }
 }
